66f3a12a0f2a96fc7a237d1b57b38aa86161c460
Ali Alawsi <87736155+Alawsi@users.noreply.github.com>
2021-07-21 14:46:29 +0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -11,20 +11,30 @@ print("""
 """)
 provider = int(input('Provider : '))
 count = int(input('Enter The count : '))
+name = input('Choose the name of the text file : ')
 
 if provider == 1:
     for i in range(count):
         num = random.randint(1000000,9999999)
         tr = random.choice(t)
-        print(f'+964{t2[0]}{tr}{num}')
+        with open(f'{name}.txt', 'a') as save:
+            save.write(f'+964{t2[0]}{tr}{num}\n')
+            save.close()
+    input('Done , Press Enter to exit ..')
 elif provider == 2:
     for i in range(count):
         num = random.randint(1000000,9999999)
         tr = random.choice(t)
-        print(f'+964{t2[1]}{tr}{num}')
+        with open(f'{name}.txt', 'a') as save:
+            save.write(f'+964{t2[1]}{tr}{num}\n')
+            save.close()
+    input('Done , Press Enter to exit ..')
 else:
     for i in range(count):
         num = random.randint(1000000, 9999999)
         tr = random.choice(t)
         t2r = random.choice(t2)
-        print(f'+964{t2r}{tr}{num}')
+        with open(f'{name}.txt', 'a') as save:
+            save.write(f'+964{t2r}{tr}{num}\n')
+            save.close()
+    input('Done , Press Enter to exit ..')
